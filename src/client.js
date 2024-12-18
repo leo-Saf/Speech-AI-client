4ccc408804196d64ee14983e1dd3ecc26ba543b4
keyjpg <93946569+keyjpg@users.noreply.github.com>
2024-12-18 10:14:12 +0100

    ..

--- src/client.js
+++ src/client.js
@@ -1,11 +1,16 @@
-export const uploadAudio = async (blob, userId) => {
+export const uploadAudio = async (blob, userId, participants = []) => {
   if (!userId) {
-    // throw new Error('Användar-ID saknas. Du måste vara inloggad.');
+    throw new Error('Användar-ID saknas. Du måste vara inloggad.');
   }
 
   const formData = new FormData();
-  formData.append('audio', blob, 'audio.webm'); // Lägg till Blob i FormData
+  formData.append('audio', blob, 'audio.webm'); // Add the Blob to the FormData
   formData.append('userId', userId);
+
+  // Serialize the participants array as a JSON string
+  if (participants.length > 0) {
+    formData.append('participants', JSON.stringify(participants));
+  }
 
   const response = await fetch('/api/process-audio', {
     method: 'POST',
@@ -16,6 +21,13 @@ export const uploadAudio = async (blob, userId) => {
     throw new Error('Fel vid uppladdning');
   }
 
-  const responseData = await response.json(); // Returnera JSON-från servern
-  return JSON.stringify(responseData); // Returnera JSON som en sträng
+  // Handle response as needed (Blob or JSON)
+  const contentType = response.headers.get('Content-Type');
+  if (contentType && contentType.includes('application/json')) {
+    const jsonResponse = await response.json();
+    return JSON.stringify(jsonResponse); // Return JSON as a string
+  } else {
+    const audioBlob = await response.blob();
+    return audioBlob; // Return processed audio as a Blob
+  }
 };
